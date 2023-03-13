a071eba711c8c106ac5cd3f7cf2da42103c56ee4
TheRealPitus <108187419+PiotrCipior@users.noreply.github.com>
2023-03-13 16:32:28 +0100

    Update Menu.cs

--- clicker_scrpits/Menu.cs
+++ clicker_scrpits/Menu.cs
@@ -7,7 +7,6 @@ public class Menu : MonoBehaviour
 {
     public void OnButtonPress()
     {
-        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
         SceneManager.LoadScene("Main");
     }
 }
