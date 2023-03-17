cd2c2e82f344006c52ae263c225aad080afcb354
TheRealPitus <108187419+PiotrCipior@users.noreply.github.com>
2023-03-17 12:13:11 +0100

    Update SCharacterController2D.cs

--- Game_jam_scripts/SCharacterController2D.cs
+++ Game_jam_scripts/SCharacterController2D.cs
@@ -3,21 +3,19 @@ using UnityEngine.Events;
 
 public class SCharacterController2D : MonoBehaviour
 {
-	[SerializeField] private float m_JumpForce = 400f;							// Amount of force added when the player jumps.
-	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
-	[SerializeField] private bool m_AirControl = false;							// Whether or not a player can steer while jumping;
-	[SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
-	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
+	[SerializeField] private float jumpForce = 400f;							// Amount of force added when the player jumps.
+	[Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;	// How much to smooth out the movement
+	[SerializeField] private bool airControl = false;							// Whether or not a player can steer while jumping;
+	[SerializeField] private LayerMask whatIsGround;							// A mask determining what is ground to the character
+	[SerializeField] private Transform groundCheck;							// A position marking where to check if the player is grounded.
 	//[SerializeField] public Transform m_CanResizeSmall;
 	//[SerializeField] public Transform m_CanResizeMedium;				// A collider that will be disabled when crouching
 
-	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-	private bool m_Grounded;            // Whether or not the player is grounded.
-	//public  bool m_CanGrowSmall2;
-	//public static bool m_CanGrowMedium2;
-	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
-	private Rigidbody2D m_Rigidbody2D;
-	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+	const float groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+	private bool grounded;            // Whether or not the player is grounded.
+	const float ceilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
+	private Rigidbody2D rigidbody2D;
+	private bool facingRight = true;  // For determining which way the player is currently facing.
 	private Vector3 velocity = Vector3.zero;
 
 	[Header("Events")]
@@ -30,7 +28,7 @@ public class SCharacterController2D : MonoBehaviour
 
 	private void Awake()
 	{
-		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+		rigidbody2D = GetComponent<Rigidbody2D>();
 		if (OnLandEvent == null)
 		{
 			OnLandEvent = new UnityEvent();
@@ -41,16 +39,16 @@ public class SCharacterController2D : MonoBehaviour
 	private void FixedUpdate()
 	{
 		bool wasGrounded = m_Grounded;
-		m_Grounded = false;
+		grounded = false;
 
 		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
-		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
 		for (int i = 0; i < colliders.Length; i++)
 		{
 			if (colliders[i].gameObject != gameObject)
 			{
-				m_Grounded = true;
+				grounded = true;
 				if (!wasGrounded)
 				{
 					OnLandEvent.Invoke();
@@ -88,24 +86,24 @@ public class SCharacterController2D : MonoBehaviour
 	
 
 		//only control the player if grounded or airControl is turned on
-		if (m_Grounded || m_AirControl)
+		if (grounded || airControl)
 		{
 
 			
 
 			// Move the character by finding the target velocity
-			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+			Vector3 targetVelocity = new Vector2(move * 10f, rigidbody2D.velocity.y);
 			// And then smoothing it out and applying it to the character
-			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref velocity, m_MovementSmoothing);
+			m_Rigidbody2D.velocity = Vector3.SmoothDamp(rigidbody2D.velocity, targetVelocity, ref velocity, movementSmoothing);
 
 			// If the input is moving the player right and the player is facing left...
-			if (move > 0 && !m_FacingRight)
+			if (move > 0 && !facingRight)
 			{
 				// ... flip the player.
 				Flip();
 			}
 			// Otherwise if the input is moving the player left and the player is facing right...
-			else if (move < 0 && m_FacingRight)
+			else if (move < 0 && facingRight)
 			{
 				// ... flip the player.
 				Flip();
@@ -115,8 +113,8 @@ public class SCharacterController2D : MonoBehaviour
 		if (m_Grounded && jump)
 		{
 			// Add a vertical force to the player.
-			m_Grounded = false;
-			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+			grounded = false;
+			rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
 		}
 	}
 
@@ -124,7 +122,7 @@ public class SCharacterController2D : MonoBehaviour
 	private void Flip()
 	{
 		// Switch the way the player is labelled as facing.
-		m_FacingRight = !m_FacingRight;
+		facingRight = !facingRight;
 
 		// Multiply the player's x local scale by -1.
 		Vector3 theScale = transform.localScale;
